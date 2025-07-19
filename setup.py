991b9b3da3de0805c6b6a3f2e681c3634abdc5e2
NgocSon-AI <286sunsun@gmail.com>
2025-07-19 16:42:59 +0700

    requirements.txt added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="SignLanguage",
+    version="0.0.0",
+    author="NgocSon",
+    author_email="286ngocson@gmail.com",
+    packages = find_packages(),
+    install_requires = []
+)
