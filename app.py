db3847cbb6ce064be5f0f6bcbef1dff880f5dab4
NgocSon-AI <286sunsun@gmail.com>
2025-07-19 17:02:49 +0700

    exception added

--- app.py
+++ app.py
@@ -1,3 +1,12 @@
 from SignLanguage.logger import logging
+from SignLanguage.exception import SignException
+
+import sys
+
 
 logging.info("Welcome to the My Project!!!")
+
+try:
+    a = 4/'9'
+except Exception as e:
+    raise SignException(e, sys) from e
